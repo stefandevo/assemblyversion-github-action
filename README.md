84b1eeb0b4d05f012f4e3b70dab5eaeff9fef4eb
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2020-03-19 22:04:09 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# assemblyversion-github-action
-Github Action for settings AssemblyVersion of csproj files .net
+# AssemblyVersion
+
+This Github Action sets the AssemblyVersion of the given csproj to the next revision/build number.
+The format is fixed for now:
+yyyy.mm.dd.xx
+
+Where xx is first read from the existing file and then increased by 1.
---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const xpath = require("xpath");
+const dom = require("xmldom").DOMParser;
+
+try {
+	let projectFile = core.getInput("project-file");
+	var text = fs.readFileSync(projectFile, "utf8");
+
+	var doc = new dom().parseFromString(text);
+	var currentVersion = xpath.select(
+		"string(/Project/PropertyGroup/AssemblyVersion)",
+		doc
+	);
+
+	const versionParts = currentVersion.split(".");
+	let revision = versionParts[versionParts.length - 1];
+	revision++;
+
+	let nextVersion = yyyymmdd() + "." + revision;
+
+	text = text.replace(
+		"<AssemblyVersion>" + currentVersion + "</AssemblyVersion>",
+		"<AssemblyVersion>" + nextVersion + "</AssemblyVersion>"
+	);
+
+	fs.writeFileSync(projectFile, text, "utf8");
+
+	core.setOutput("version", nextVersion);
+} catch (error) {
+	core.setFailed(error.message);
+}
+
+function yyyymmdd() {
+	function twoDigit(n) {
+		return (n < 10 ? "0" : "") + n;
+	}
+
+	var now = new Date();
+	return (
+		"" +
+		now.getFullYear() +
+		"." +
+		twoDigit(now.getMonth() + 1) +
+		"." +
+		twoDigit(now.getDate())
+	);
+}
